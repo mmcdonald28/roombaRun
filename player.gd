81cef4e5cac9a489b112d7d3acddc70e34be64bb
nMuhlenberg <nmichaels@muhlenberg.edu>
2023-10-30 13:39:27 -0400

    add animation

--- player.gd
+++ player.gd
@@ -13,10 +13,22 @@ func _physics_process(delta):
 	get_input()
 	move_and_slide()
 
+func _input(event):
+	if(event.is_action_pressed("left")):
+		$Icon.texture = load("res://Art/icon.png")
+		
+	if(event.is_action_pressed("right")):
+		$Icon.texture = load("res://Art/icon - RIght.png")
+		
+	if(event.is_action_pressed("up")):
+		$Icon.texture = load("res://Art/icon - Up.png")
+		
+	if(event.is_action_pressed("down")):
+		$Icon.texture = load("res://Art/icon - Down.png")
 	
-func _on_Player_body_entered(body):
-	hide()
-	if body.is_in_group("Collectibles"):
-		print("Collision!")
-		body.queue_free()  # Remove the collectible
+#func _on_Player_body_entered(body):
+	#hide()
+	#if body.is_in_group("Collectibles"):
+		#print("Collision!")
+		#body.queue_free()  # Remove the collectible
 			# Add to inventory:
