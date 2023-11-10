330826852d768d2ce1fa5f60cdef8a9e5b13b49f
nMuhlenberg <nmichaels@muhlenberg.edu>
2023-11-10 12:35:56 -0500

    fixed timer format

--- TimerPanel.gd
+++ TimerPanel.gd
@@ -1,21 +1,21 @@
 extends Panel
 
-var time: float = 0.0
-var mins: int = 0
-var secs: int = 0
-var millis: int = 0
+var time: float = 0.0 #time
+var mins: int = 0 #minutes
+var secs: int = 0 #seconds
+var centis: int = 0 #centiseconds
 
 func _process(delta) -> void:
-	time += delta
-	millis = fmod(time, 1) * 100
-	secs = fmod(time, 60)
-	mins = fmod(time, 3600) / 60
-	$Minutes.text = "%02d:" % mins
-	$Seconds.text = "%02d." % secs
-	$Milliseconds.text = "%03d" % millis
+	time += delta #updating the time with delta
+	centis = fmod(time, 1) * 100 # making the centiseconds
+	secs = fmod(time, 60) # making the seconds
+	mins = fmod(time, 3600) / 60 # making the minutes
+	$Minutes.text = "%02d:" % mins # showing minutes
+	$Seconds.text = "%02d." % secs # showing seconds
+	$Centiseconds.text = "%3d" % centis # showing centiseconds
 	
 func stop() -> void:
 	set_process(false)
 	
 func get_time_formatted() -> String:
-	return "02%d:02%d.03%d" % [mins, secs, millis]
+	return "02%d:02%d.3%d" % [mins, secs, centis] # revealing the format of the timer
