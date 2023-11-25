c70523b557e8e3bf76b78130fe92fd589760f26c
SerenaMonteforte <2021montefse@gmail.com>
2023-11-25 17:21:35 -0500

    Maze 3 Collision

--- menu.gd
+++ menu.gd
@@ -15,7 +15,7 @@ func _on_quit_pressed():
 
 
 func _on_maze_pressed():
-	get_tree().change_scene_to_file("res://maze.tscn")
+	get_tree().change_scene_to_file("res://Game Levels/maze_3.tscn")
 
 
 func _on_tutorial_pressed():
